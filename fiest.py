44f16d6d90a8e39f819a35ea32460563ee25bf50
DJDhana <dhanasekharmarolix@gmail.com>
2023-08-07 12:41:54 +0530

    this my code

--- fiest.py
+++ fiest.py
@@ -24,7 +24,7 @@
 # take a string repat it 5 times
 
 #i=0
-#while i<=5:
+#while i<=5: 
     #print("DHANASEKHAR")
     #i+=1
 
@@ -94,3 +94,110 @@
     #for k in range (0,i+1):
         #print("*",end="")
     #print(  )
+
+
+####1.Write a python program to remove character from string    
+
+
+#D = "Dhana sekhar is a very gud boy "
+#D1 =D.replace("very","")
+#print(D1)
+
+# 2.Write a program to check String is Palindrome or not
+
+#string = input("enter a string:")
+#revstr = ""
+#for i in string:
+    #revstr = i + revstr
+#if (string == revstr):
+    #print("the given string is palindrome")
+#else:
+    #print ("the given string is not a palindrome")
+
+
+#3.Write a python program to check given character is vowel or consonant
+
+#D = input("enter a char:")
+#if (D == 'a' or D == 'e' or D == 'i' or D == 'o' or D == 'u' or D == 'A' or D == 'E' or D == 'I' or D == 'O' or D == 'U'):
+ #   print(D,"is a vowles")
+#else:
+#    print(D,"is a consonant")
+
+
+# 4.Write a python program to replace string space with given character in Python
+
+#s="am working in marolix"
+#s2=s.replace(" ","the")
+#print(s2)
+
+#5.Write a python program to count alphabets, digits, and special characters in the string
+
+#string = input("Enter a String : ")
+#alphabets=0
+#digits=0
+#specialChars=0
+#for i in string: 
+    		#if i.isalpha():
+       			 #alphabets+=1
+    		#elif i.isdigit():
+        			#digits+=1
+    		#else:
+        			#specialChars+=1
+#print("alphabets =",alphabets,"digits =",digits,"specialChars =",specialChars)
+
+#6.Write a python program to remove all the blank spaces in a given string
+#string = "coders hub  "
+#new_string = string.replace(" ", "")
+#print(new_string)
+
+#7.Write a python program to find sum of integers in the string
+str1 = input('Enter a string: ')
+sum=0
+for i in str1:
+    if i.isdigit(): 
+        sum=sum+int(i)
+print("sum=",sum)
+
+
+#8.Write a python program to Remove Repeated Character from String.
+#string="marolix technology solution"
+#p=""
+#for char in string:
+	#if char not in p:
+		#p=p+char
+#print(p)
+
+#9.Write a python program to count occurrence of given character in string.
+
+#count = 0
+#my_string = "dhanasekhar"
+#my_char = "a"
+#for i in my_string:
+    #if i == my_char:
+        #count += 1
+#print(count)
+
+#10.Write a python program to check string is anagrams or not in Python.
+#str1 = "race"
+#str2 = "care"
+#if(len(str1) == len(str2)):
+    #sorted_str1 = sorted(str1)
+    #sorted_str2 = sorted(str2)
+    #if(sorted_str1 == sorted_str2):
+        #print(str1 + " and " + str2 + " are anagram.")
+    #else:
+        #print(str1 + " and " + str2 + " are not anagram.")
+#else:
+    #print(str1 + " and " + str2 + " are not anagram.")
+
+#11.Write a Program to Sort the Characters of the String and First Alphabet Symbols followed by Numeric Values.
+#string=input("Enter a string: ")
+#alpha=[] 
+#digit=[] 
+#for c in string:
+    #if c.isalpha():
+        #alpha.append(c)  
+    #else:
+        #digit.append(c)
+#result=''.join(sorted(alpha)+sorted(digit)) 
+#print(result)
