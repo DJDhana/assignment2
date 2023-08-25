1e10b58db5326f6af71cf43b13e58c0abc52d1bf
DJDhana <dhanasekharmarolix@gmail.com>
2023-08-25 12:19:01 +0530

    assignment4

---
+++ second.py
@@ -0,0 +1,75 @@
+## write a program to merge two list
+
+# x = ["usha","gurrappa","likitha"]
+# y = ["dhana","sekhar"]
+# x.extend(y)
+# print(x)
+
+#how to find sum of list
+
+#num = [10,20,30,40,50]
+#print(sum(num))
+
+#print  even numbers from list
+
+# d = [10,20,30,40,5,24,71,21,34,56,87,96,113]
+# list =[]
+# for i in d:
+#     if i % 2 == 0:
+#         list.append(i)
+# print(list)
+        
+
+# print odd numbers given list
+
+# d = [10,20,30,40,5,24,71,21,34,56,87,96,113]
+# list =[]
+# for i in d: 
+#     if i % 2 != 0:
+#         list.append(i)
+# print(list)
+        
+
+## delete element of given index
+
+# j = ["dhana","sekhar","usha","likitha","gurrappa"]
+# j.remove("sekhar")
+# print(j)
+
+## delete element directly from a given list
+
+# j = ["dhana","sekhar","usha","likitha","gurrappa"]
+# j.pop(1)
+# print(j)
+
+# inser a element at a given index 
+
+# l = ["Dhana","sekhar","likitha","gurrappa"]
+# l.insert(3,"usha")
+# print(l)
+
+# inser a element at a given index at a end
+
+# l = ["Dhana","sekhar","likitha","gurrappa"]
+# l.append("usha")
+# print(l)
+
+
+## check the size of two list are same or not 
+
+# lst1=int(input("enter the size1:" ))
+# lst2=int(input("enter the size2:"))
+# new_list1=[]
+# new_list2=[]
+# for i in range(0,lst1):
+#    element1=int(input("enter the element:"+str(i)+":"))
+#    new_list1.append(element1)
+# print(new_list1)
+# for j in range(0,lst2):
+#   element2=int(input("enter the element:"+str()+":"))
+#   new_list2.append(element2)
+# print(new_list2)
+# if len(new_list1)==len(new_list2):
+#     print("length are equal")
+# else:
+#     print("length are not equal")
